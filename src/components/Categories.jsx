5ba7288e7addb3db8ee96a1b64025db94fe5df30
sebastian <sebaxtian098@hotmail.com>
2019-12-02 17:07:50 -0500

    nuevos-comomnets

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import '../assets/styles/components/Categories.scss';
 
-const Categories = ( {children} ) => (
+const Categories = ({children}) => (
     <div className="categories">
         <h3 class="categories__title">Mi lista</h3>
         {children}
-    </div>
-    
+    </div> 
 );
 
 export default Categories;
